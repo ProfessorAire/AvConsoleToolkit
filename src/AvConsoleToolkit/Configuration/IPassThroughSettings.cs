38088511014f7563b6c65ae35b9a280f355968f5
ProfessorAire <ProfessorAire@users.noreply.github.com>
2025-11-25 21:19:47 -0800

    chore: Improved history and scrolling through previous commands.

---
+++ src/AvConsoleToolkit/Configuration/IPassThroughSettings.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace AvConsoleToolkit.Configuration
+{
+    public interface IPassThroughSettings
+    {
+        /// <summary>
+        /// Gets a value indicating whether to use command history for pass-through sessions.
+        /// </summary>
+        [DefaultValue(true)]
+        bool UseHistoryForPassThrough { get; set; }
+    }
+}
