23ccbee7b26c1d80724a7daa90006855843e0a21
ProfessorAire <ProfessorAire@users.noreply.github.com>
2025-12-01 12:35:27 -0800

    chore: Fixed broken reconnection logic for the pass through commands.

--- src/AvConsoleToolkit/Configuration/AppConfig.cs
+++ src/AvConsoleToolkit/Configuration/AppConfig.cs
@@ -1,7 +1,7 @@
 // <copyright file="AppConfig.cs">
 // The MIT License
 // Copyright © Christopher McNeely
-// Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the “Software”),
+// Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"),
 // to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense,
 // and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
 // The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
@@ -51,11 +51,11 @@ namespace AvConsoleToolkit.Configuration
             builder.UseIniFile(userPath)
             .UseInMemoryDictionary(new Dictionary<string, string>
             {
-            // Default settings are defined here
-            {
-                "Connection.AddressBooksLocation",
-                Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), "Crestron", "ToolBox", "AddressBook")
-            }
+                // Default settings are defined here
+                {
+                    "Connection.AddressBooksLocation",
+                    Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), "Crestron", "ToolBox", "AddressBook")
+                },
             })
             .Build();
 
